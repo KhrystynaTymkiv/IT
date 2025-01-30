5f5a39f926c68b8e637383002405c2f0a48ac6c2
Khrystyna <tymkivhrystyna007@icloud.com>
2025-01-30 14:13:56 +0200

    first committtt

---
+++ main.py
@@ -0,0 +1,23 @@
+# 1
+# def greet():
+#     print("Привіт, світ!")
+#
+# greet()
+
+# 2
+# def add_numbers(a, b):
+#     return a + b
+#
+# a = float(input("Введіть перше число: "))
+# b = float(input("Введіть друге число: "))
+# result = add_numbers(a, b)
+# print("Сума чисел:", result)
+
+# 3
+def rectangle_area(width, height):
+    return width * height
+
+width = float(input("Введіть ширину прямокутника: "))
+height = float(input("Введіть висоту прямокутника: "))
+area = rectangle_area(width, height)
+print("Площа прямокутника:", area)
